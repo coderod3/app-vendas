74807bb5934e99b118dd80859d577c08cf182c70
coderod3 <rodrigo.araujo9013@gmail.com>
2026-06-06 05:39:36 -0300

    fix: mudança de modelo

--- src/app/api/processar-venda/route.js
+++ src/app/api/processar-venda/route.js
@@ -23,7 +23,7 @@ export async function POST(request) {
     const base64Audio = Buffer.from(arrayBuffer).toString("base64");
 
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-1.5-flash-002",
+      model: "gemini-2.5-flash",
       generationConfig: { responseMimeType: "application/json" }
     });
 
